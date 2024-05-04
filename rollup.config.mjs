cfa5e1aa92695904a1621c08e6e3556774aa5f0e
jarvisjiang <jarvisjiang@tencent.com>
2024-05-04 15:53:27 +0000

    replace parcel build with rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,45 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+const input = 'src/mod.ts';
+const external = [
+    /^@std\/path/,
+    '@happy-js/happy-rusty',
+];
+
+/**
+ * @type {import('rollup').RollupOptions[]}
+ */
+export default [
+    {
+        input,
+        plugins: [
+            esbuild(),
+        ],
+        output: [
+            {
+                file: 'dist/main.cjs',
+                format: 'cjs',
+                sourcemap: true,
+            },
+            {
+                file: 'dist/main.mjs',
+                format: 'esm',
+                sourcemap: true,
+            },
+        ],
+        external,
+    },
+    {
+        input,
+        plugins: [
+            dts(),
+        ],
+        output: {
+            file: 'dist/types.d.ts',
+            format: 'esm',
+            sourcemap: true,
+        },
+        external,
+    },
+];
