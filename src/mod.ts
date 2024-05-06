278b279b0b487375958f41419c7d6ec03e5f19f8
jarvisjiang <jarvisjiang@tencent.com>
2024-05-06 10:11:35 +0000

    export `NOT_FOUND_ERROR`

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,5 @@
 export { assertAbsolutePath, assertFileUrl } from './fs/assertions.ts';
+export { NOT_FOUND_ERROR } from './fs/constants.ts';
 export { FileEncoding, type ExistsOptions, type FsAsyncResult, type ReadFileContent, type ReadOptions, type WriteFileContent, type WriteOptions } from './fs/defines.ts';
 export { isCurrentDir, isOPFSSupported, isRootPath } from './fs/helpers.ts';
 export { mkdir, readDir, readFile, remove, rename, stat, writeFile } from './fs/opfs_core.ts';
