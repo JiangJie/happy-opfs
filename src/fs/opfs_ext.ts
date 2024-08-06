2c48d28054398e8fec79b63bdbe53b754a42c38e
jarvisjiang <jarvisjiang@tencent.com>
2024-08-06 07:11:17 +0000

    add `unzip` method

--- src/fs/opfs_ext.ts
+++ src/fs/opfs_ext.ts
@@ -1,6 +1,8 @@
 import { fetchT, type FetchResponse, type FetchTask } from '@happy-ts/fetch-t';
 import { basename, join } from '@std/path/posix';
-import { Err, Ok, RESULT_FALSE, RESULT_VOID, type AsyncIOResult, type AsyncVoidIOResult, type IOResult } from 'happy-rusty';
+import * as fflate from 'fflate/browser';
+import { Err, Ok, RESULT_FALSE, RESULT_VOID, type AsyncIOResult, type AsyncVoidIOResult, type IOResult, type VoidIOResult } from 'happy-rusty';
+import { Future } from 'tiny-future';
 import invariant from 'tiny-invariant';
 import { assertAbsolutePath, assertFileUrl } from './assertions.ts';
 import { ABORT_ERROR } from './constants.ts';
@@ -226,4 +228,46 @@ export function uploadFile(filePath: string, fileUrl: string, requestInit?: Uplo
             return response;
         },
     };
+}
+
+/**
+ * Unzip a zip file to a directory.
+ * @param zipFilePath - Zip file path.
+ * @param targetPath - The directory to unzip to.
+ * @returns A promise that resolves to an `AsyncIOResult` indicating whether the zip file was successfully unzipped.
+ */
+export async function unzip(zipFilePath: string, targetPath: string): AsyncVoidIOResult {
+    assertAbsolutePath(targetPath);
+
+    const res = await readFile(zipFilePath);
+    if (res.isErr()) {
+        return res.asErr();
+    }
+
+    const future = new Future<VoidIOResult>();
+
+    const data = new Uint8Array(res.unwrap());
+
+    fflate.unzip(data, async (err, unzipped) => {
+        if (err) {
+            future.resolve(Err(err));
+            return;
+        }
+
+        const tasks: AsyncVoidIOResult[] = [];
+        for (const path in unzipped) {
+            // ignore directory
+            if (path.at(-1) !== '/') {
+                tasks.push(writeFile(join(targetPath, path), unzipped[path]));
+            }
+        }
+
+        const allRes = await Promise.all(tasks);
+        // anyone failed?
+        const fail = allRes.find(x => x.isErr());
+
+        return future.resolve(fail ?? RESULT_VOID);
+    });
+
+    return await future.promise;
 }
