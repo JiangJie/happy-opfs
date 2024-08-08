e119b7d7f6ebbf0999c7241cb3adebea9ec8e60e
jarvisjiang <jarvisjiang@tencent.com>
2024-08-08 06:09:38 +0000

    add `pruneTemp` to delete expired files

--- src/fs/opfs_tmp.ts
+++ src/fs/opfs_tmp.ts
@@ -1,8 +1,9 @@
 import { join } from '@std/path/posix';
-import { Ok, type AsyncIOResult, type AsyncVoidIOResult } from 'happy-rusty';
+import { Err, Ok, RESULT_VOID, type AsyncIOResult, type AsyncVoidIOResult } from 'happy-rusty';
 import { TMP_DIR } from './constants.ts';
 import type { TempOptions } from './defines.ts';
-import { createFile, mkdir, remove } from './opfs_core.ts';
+import { createFile, mkdir, readDir, remove } from './opfs_core.ts';
+import { isFileHandle } from './utils.ts';
 
 /**
  * Generate a temporary path but not create it.
@@ -47,4 +48,33 @@ export async function mkTemp(options?: TempOptions): AsyncIOResult<string> {
  */
 export function deleteTemp(): AsyncVoidIOResult {
     return remove(TMP_DIR);
+}
+
+/**
+ * Prune the temporary directory and delete all expired files.
+ * @param expired - The date to determine whether a file is expired.
+ * @returns A promise that resolves to an `AsyncVoidIOResult` indicating whether the temporary directory was successfully pruned.
+ */
+export async function pruneTemp(expired: Date): AsyncVoidIOResult {
+    const res = await readDir(TMP_DIR, {
+        recursive: true,
+    });
+
+    if (res.isErr()) {
+        return res.asErr();
+    }
+
+    try {
+        for await (const { handle } of res.unwrap()) {
+            if (isFileHandle(handle) && (await handle.getFile()).lastModified <= expired.getTime()) {
+                // TODO ts not support yet
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                await (handle as any).remove();
+            }
+        }
+    } catch (e) {
+        return Err(e as DOMException);
+    }
+
+    return RESULT_VOID;
 }
