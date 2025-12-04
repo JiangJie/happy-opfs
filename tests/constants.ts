10f9b931b173577396fb4f0571d014b25a53127e
jarvisjiang <jarvisjiang@tencent.com>
2025-12-04 23:11:12 +0800

    test: remove legacy browser-based test infrastructure
    The old manual browser test files have been replaced by the new
    Vitest and Playwright test suites added in previous commits.

--- tests/constants.ts
+++
@@ -1,6 +0,0 @@
-const mockServer = 'https://fakestoreapi.com';
-
-export const mockAll = `${ mockServer }/products`;
-export const mockSingle = `${ mockAll }/1`;
-
-export const mockZipUrl = 'https://raw.githubusercontent.com/JiangJie/happy-opfs/main/tests/test.zip';
