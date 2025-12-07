91c8df0424572fa46148ac31321fc571f6389b78
jarvisjiang <jarvisjiang@tencent.com>
2025-12-07 16:22:38 +0800

    chore(eslint): ignore coverage directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores([
+        'dist',
+        'coverage',
+    ]),
     {
         files: ['**/*.ts'],
         extends: [
